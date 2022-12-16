38fa105eb436ae2e6a3407c02427cdaa9e24315d
Piriya <piriya.muaithaisong@gmail.com>
2022-12-17 00:31:30 +0700

    Typing serach-box

---
+++ src/components/search-box/search-box.component.tsx
@@ -0,0 +1,16 @@
+import './search-box.styles.css'
+import { ChangeEventHandler } from 'react';
+
+type SearchBoxProps = {
+    className: string;
+    placeholder?: string;
+    onChangeHandler: ChangeEventHandler<HTMLInputElement>
+}
+
+const SearchBox = ({ className, placeholder, onChangeHandler }: SearchBoxProps) => (
+    <div>
+        <input className={`search-box ${className}`} type='search' placeholder={placeholder} onChange={onChangeHandler} />
+    </div>
+);
+
+export default SearchBox;
